fa43d732eda6140f1402c052c3f089929efa533a
yaranir <yara.duarte.santos@escola.pr.gov.br>
2025-09-04 09:05:11 -0300

    ok

---
+++ script.js
@@ -0,0 +1,14 @@
+const avança = document.querySelectorAll('btn-proximo');
+const reiniciarBtn = document.getElementById('.btn-reiniciar');
+
+avança.forEach(button => {
+button.addEventListener('click',function(){
+    const atual = document.querySelector('.ativo');
+    const proximoPasso='passo-'+this.getAttribute('data-proximo');
+})
+
+
+}
+
+
+)
