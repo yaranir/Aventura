347c36e974677f4558d1cac55165f2b871b1a339
yaranir <yara.duarte.santos@escola.pr.gov.br>
2025-09-11 08:45:47 -0300

    ok

--- script.js
+++ script.js
@@ -1,14 +1,23 @@
 const avança = document.querySelectorAll('btn-proximo');
 const reiniciarBtn = document.getElementById('.btn-reiniciar');
+avanca.forEach(button => {
+    button.addEventListener('click', function () {
+        const atual = document.querySelector('.ativo');
+        const proximoPasso = 'passo-' + this.getAttribute('data-proximo');
+        atual.classList.remove('ativo');
+        const proximoElemento = document.getElementById(proximoPasso);
+        const avanca = document.querySelectorAll('btn.proximo');
+        const reiniciarBtn = document.getElementById('btn-reiniciar');
+        avanca.forEach(button => {
+            button.addEventListener('click', function () {
+                const atual = document.querySelector('.ativo');
+                const proximoPasso = 'passo-' + this.getAttribute('data-proximo');
+                if (proximoElemento) {
+                    proximoElemento.classList.add('ativo');
+                } else {
+                    console, console.error(`Elemento com ID "${proximoPasso}" não encontrado.`);
+                }
 
-avança.forEach(button => {
-button.addEventListener('click',function(){
-    const atual = document.querySelector('.ativo');
-    const proximoPasso='passo-'+this.getAttribute('data-proximo');
-})
-
-
-}
-
-
-)
+        }}
+    )
+        });
